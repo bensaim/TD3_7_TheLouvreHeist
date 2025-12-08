26a145f214181f3014375e6891519313d48aa955
bensaim <claire.bensaim@etu.univ-smb.fr>
2025-12-08 14:12:52 +0100

    Ajoutez des fichiers projet.

---
+++ LouvreHeist/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace LouvreHeist
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
